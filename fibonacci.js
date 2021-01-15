91bf57e5eddff761d0eadb90265cd54434df6ae5
Danny Tri <cookieduck9012@gmail.com>
2021-01-15 17:34:52 +1100

    Added if statement

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,9 @@
 //This is fibonacci challenge
 let results = []
 for (let num = 0; num < 10; num++) {
-   
+   if (results.length == 0) {
+       return results.push(0, 1)
+   } 
   }
 console.log(results)
+console.log("Hello World");
---
+++ sumif.js
@@ -0,0 +1,3 @@
+// Create a function called sumIfPossible. :D 
+// This function should take an array as input, and sum the values together if possible. :D
+// If the variables are not the correct data type, skip over it, but keep track of how many skips have occurred
